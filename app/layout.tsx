a0964464c3f47cee5d173b8eba987489f3a8bc60
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-12-31 01:12:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Providers } from './providers';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,9 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
 }
-
